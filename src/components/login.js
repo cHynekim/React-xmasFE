208b1360b218222f6188557e1d53c0b9c823c248
cHynekim <liaco819@naver.com>
2023-12-06 08:20:01 +0900

    061223_react_loginFE

---
+++ src/components/login.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import LogBtn from './loginBtn.js';
+import '../res/style/login.css'
+
+const Login = () => {
+    const [cont, setCont] = useState([
+        {idx : 0, txt : "내 노트북 찾기", bg : '#1FB4A2', clickBg : '#218E80'},
+        {idx : 1, txt : "새 노트북 만들기", bg : '#f66', clickBg : '#D25C5C'}
+    ]);
+    const [toggle, setToggle] = useState(false);
+
+    return(
+        <div className='body'>
+            <div className='modal'>
+                <div className='subtitle'>
+                    <p className='sub-default'>
+                        크리스마스에도 애인, 아니?<br/>
+                        <span className='sub-hl'>노트북</span>
+                        과 함께할 그대들을 위한
+                        <span className='sub-hl'> 편지 서비스💝</span>
+                    </p>
+                </div>
+                <div className='maintitle'>
+                    <span className='title'>엥? 노트북을 꾸미라고?</span>
+                </div>
+                <LogBtn
+                    data={cont}
+                    toggle={toggle}
+                    onChangeToggle={()=>{
+                        setToggle(!toggle);
+                    }}
+                ></LogBtn>
+            </div>
+        </div>
+    );
+}
+export default Login;
---
+++ src/components/loginBtn.js
@@ -0,0 +1,52 @@
+import { useState, useRef } from "react";
+import '../res/style/loginBtn.css';
+import kakao from '../res/img/kakao.png';
+import naver from '../res/img/naver.png';
+import google from '../res/img/google.png';
+
+const LogBtn = (props) => {
+    const [clickedIdx, setClickedIdx] = useState(null);
+    const [readySocial, setReadySocial] = useState(null);
+    const [showSocial, setShowSocial] = useState('mount');
+
+    const doBtn = props.data.map((data) => {
+        return(
+            <div className="btn-cover" key={data.idx}>
+                <button 
+                    className="btn"
+                    style={{ backgroundColor: data.idx === clickedIdx ? data.clickBg : data.bg }}
+                    key={data.idx}
+                    type="submit"
+                    onClick={() => {
+                        props.onChangeToggle();
+                        if(!props.toggle){
+                            console.log(props.toggle);
+                            setShowSocial('mount');
+                        }
+                        // else{
+                        //     setShowSocial('unmount');
+                        // }
+                        setReadySocial(data.idx);
+                        setClickedIdx(data.idx);
+                        setTimeout(() => setClickedIdx(null), 100);
+                }}>
+                    {data.txt}
+                </button>
+                {readySocial === data.idx && (
+                    <div className={`social-cover ${showSocial}`}>
+                        <a key={"kakao"} className="social-option" onClick={(e)=>{e.preventDefault();}}><img className="social-img" src={kakao}></img></a>
+                        <a key={"naver"} className="social-option" onClick={(e)=>{e.preventDefault();}}><img className="social-img" src={naver}></img></a>
+                        <a key={"google"} className="social-option" onClick={(e)=>{e.preventDefault();}}><img className="social-img" src={google}/></a>
+                    </div>
+                )}
+            </div>
+        );
+    });
+
+    return(
+        <div className="btn-section">
+            {doBtn}
+        </div>
+    );
+}
+export default LogBtn;
